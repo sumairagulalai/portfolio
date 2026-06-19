2748725844bb2fce65f6e84d87c0699a328adf7d
sumairagulalai <sumairakhan112005@gmail.com>
2026-06-19 14:35:13 +0500

    Delete sumi.html directory

--- sumi.html/methods.py
+++
@@ -1,6 +0,0 @@
-# methods of strings 
-# endswith,capitilize,str.find,str.count,str.replace(old,new)
-# methods of lists 
-#append,sort,sort(reverse=True),reverse,insert,pop(1 specific),remove(first element)
-# methods of tuple 
-# count(),index()=>index find the index offirst occurance element
